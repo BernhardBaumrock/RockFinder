def88e28d27ecfa027ba0a41a5e1557c88f063ed
Bernhard Baumrock <bernhard@baumrock.com>
2018-12-18 21:22:55 +0100

    bump version

--- RockFinder.info.php
+++ RockFinder.info.php
@@ -1,7 +1,7 @@
 <?php
 $info = [
   'title' => 'RockFinder',
-  'version' => '1.0.9',
+  'version' => '1.1.0',
   'summary' => 'Highly Efficient and Flexible SQL Finder Module to return page data without loading PW pages into memory',
   'singular' => true, 
   'autoload' => false, 
